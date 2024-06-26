607fde8eb802c2f9a8a30bb52e50266174f3869d
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-06-26 21:35:41 +0500

    test

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,7 +4,7 @@ export default async function SingleMoviePage({
   params: { id: string };
 }) {
   const res = await fetch(
-    `https://api.cinerama.uz/test/movies/view?module_id=3&id=${43657}`,
+    `https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`,
     {
       headers: {
         Authorization: `Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM`,
