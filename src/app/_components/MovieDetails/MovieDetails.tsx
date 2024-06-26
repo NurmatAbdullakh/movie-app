43526cad23120aa412dbeeb3da142c1d19132aee
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-06-26 15:09:17 +0500

    feat : add custom video player with player-react

--- src/app/_components/MovieDetails/MovieDetails.tsx
+++ src/app/_components/MovieDetails/MovieDetails.tsx
@@ -1,86 +1,57 @@
+"use client";
 import { IMovieDetails } from "@/api/movies/movies.types";
 import styles from "./MovieDetails.module.scss";
-import Image from "next/image";
 import Actors from "../Actors/Actors";
 import SectionTitle from "../SectionTitle/Sectiontitle";
+import dynamic from "next/dynamic";
+import Badges from "../Badges/Badges";
+
+const VideoPlayer = dynamic(() => import("../VideoPlayer/VideoPlayer"), {
+  ssr: false,
+});
 
 interface props {
   details: IMovieDetails | undefined;
 }
+
 const MovieDetails = ({ details }: props) => {
-  console.log("details", details);
-
   return (
-    <div className={styles.movie_details}>
-      <div className={styles.movie_details__container}>
-        <div className={styles.movie_details__banner}>
-          <Image
-            className={styles.movie_details__overlay}
-            src={details?.poster || ""}
-            alt={details?.title || ""}
-            fill
-          />
-          <Image
-            src="/images/overlay.png"
-            className={styles.movie_details__overlay}
-            alt="overlay"
-            fill
-          />
-          <Image
-            className={styles.movie_details__poster}
-            src={details?.poster || ""}
-            alt={details?.title || ""}
-            fill
-          />
-        </div>
-        <h1 className={styles.movie_details__title}>{details?.title}</h1>
-        <div className={`${styles.movie_details__year} xl-500`}>
-          {details?.year}
-        </div>
-        <div className={`${styles.movie_details__description} md`}>
-          {details?.description}
-        </div>
+    <>
+      <div className={styles.movie_details}>
+        <div className={styles.movie_details__container}>
+          <div className={styles.movie_details__banner}>
+            <VideoPlayer />
+          </div>
+          <h1 className={styles.movie_details__title}>{details?.title}</h1>
+          <div className={`${styles.movie_details__year} xl-500`}>
+            {details?.year}
+          </div>
+          <div className={`${styles.movie_details__description} md`}>
+            {details?.description}
+          </div>
 
-        {details?.genres && (
-          <>
-            <SectionTitle title="Жанр" />
+          {details?.genres && (
+            <>
+              <SectionTitle title="Жанр" />
+              <Badges data={details?.genres} />
+            </>
+          )}
 
-            <div className={styles.movie_details__genres_list}>
-              {details?.genres?.map((genre) => (
-                <p
-                  key={genre.id}
-                  className={`${styles.movie_details__badge} xs`}
-                >
-                  {genre.title}
-                </p>
-              ))}
-            </div>
-          </>
-        )}
-
-        {details?.countries && (
-          <>
-            <SectionTitle title="Страна" />
-            <div className={styles.movie_details__country}>
-              {details?.countries?.map((genre) => (
-                <p
-                  key={genre.id}
-                  className={`${styles.movie_details__badge} xs`}
-                >
-                  {genre.title}
-                </p>
-              ))}
-            </div>
-          </>
-        )}
-        {details?.actors && (
-          <>
-            <SectionTitle title="Актеры" />
-            <Actors actors={details?.actors} />
-          </>
-        )}
+          {details?.countries && (
+            <>
+              <SectionTitle title="Страна" />
+              <Badges data={details?.countries} />
+            </>
+          )}
+          {details?.actors && (
+            <>
+              <SectionTitle title="Актеры" />
+              <Actors actors={details?.actors} />
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/app/_components/VideoPlayer/VideoPlayer.tsx
@@ -0,0 +1,84 @@
+import { useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import styles from "./VideoPlayer.module.scss";
+
+const VideoPlayer = () => {
+  const [playing, setPlaying] = useState(true);
+  const [playedSeconds, setPlayedSeconds] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const playerRef = useRef<ReactPlayer>(null);
+
+  const handleTogglePlayPause = () => {
+    if (playerRef.current) {
+      if (playing) {
+        playerRef.current.getInternalPlayer().pauseVideo();
+      } else {
+        playerRef.current.getInternalPlayer().playVideo();
+      }
+      setPlaying(!playing);
+    }
+  };
+
+  const handleProgress = (state: { playedSeconds: number; played: number }) => {
+    setPlayedSeconds(state.playedSeconds);
+  };
+
+  const handleDuration = (duration: number) => {
+    setDuration(duration);
+  };
+
+  const handleSeekChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const seekTime = parseFloat(e.target.value);
+    if (playerRef.current) {
+      playerRef.current.seekTo(seekTime);
+      setPlayedSeconds(seekTime);
+    }
+  };
+
+  const formatDuration = (seconds: number) => {
+    const mm = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const ss = Math.floor(seconds % 60)
+      .toString()
+      .padStart(2, "0");
+    return `${mm}:${ss}`;
+  };
+
+  return (
+    <div style={{ position: "relative", width: "100%", height: "500px" }}>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=U8XH3W0cMss"
+        controls={true}
+        width="100%"
+        height="100%"
+        ref={playerRef}
+        muted={true}
+        playing={playing}
+        onProgress={handleProgress}
+        onDuration={handleDuration}
+      />
+      <div className={styles.controls}>
+        <button
+          className={styles["playing-btn"]}
+          onClick={handleTogglePlayPause}
+        >
+          {playing ? "Pause" : "Play"}
+        </button>
+        <input
+          className={styles.progressbar}
+          type="range"
+          min={0}
+          max={duration}
+          value={playedSeconds}
+          onChange={handleSeekChange}
+        />
+        <span style={{ marginLeft: "10px" }}>
+          {formatDuration(playedSeconds)} / {formatDuration(duration)}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPlayer;
