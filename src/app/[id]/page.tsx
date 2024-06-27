996a1e4f74885c3b4da402b20276ae232af297b5
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-06-27 08:19:29 +0500

    add error handling

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -3,16 +3,28 @@ export default async function SingleMoviePage({
 }: {
   params: { id: string };
 }) {
-  const res = await fetch(
-    `https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`,
-    {
-      headers: {
-        Authorization: `Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM`,
-      },
-      cache: "force-cache",
-    }
-  );
-  const data = await res.json();
+  try {
+    const res = await fetch(
+      `https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`,
+      {
+        headers: {
+          Authorization: `Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM`,
+          "Content-Type": "application/json",
+        },
+        cache: "no-cache",
+      }
+    );
 
-  return <>{JSON.stringify(data)}</>;
+    if (!res.ok) {
+      throw new Error(`Server-side fetch failed with status ${res.status}`);
+    }
+
+    const data = await res.json();
+    console.log("Server-side fetch data:", data);
+
+    return <>{JSON.stringify(data)}</>;
+  } catch (error: any) {
+    console.error("Server-side fetch error:", error.message);
+    return <>Error: {error.message}</>;
+  }
 }
