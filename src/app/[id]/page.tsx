f3299fc6e6a3016699842d1372bfc77d216abe2a
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-06-27 17:22:12 +0500

    remove : useless lines

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -6,12 +6,8 @@ interface Params {
   id: string;
 }
 
-// Define the server component for fetching and displaying the movie data
 const SingleMoviePage: FC<{ params: Params }> = async ({ params }) => {
   try {
-    console.log("Fetching movie with ID:", params.id);
-
-    // Fetch the movie data
     const res = await fetch(
       `https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`,
       {
@@ -26,14 +22,10 @@ const SingleMoviePage: FC<{ params: Params }> = async ({ params }) => {
       throw new Error(`Server-side fetch failed with status ${res.status}`);
     }
 
-    // Parse the JSON response
     const data: IMovieDetailsResponse = await res.json();
-    console.log("Fetched data:", data);
 
-    // return <>{JSON.stringify(data)}</>;
     return <MovieDetails details={data.data} />;
   } catch (error: any) {
-    console.error("Fetch error:", error.message);
     return <div>Error: {error.message}</div>;
   }
 };
