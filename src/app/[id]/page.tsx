72f658133113aaad3ea56986702393fa7c52ecb0
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-06-28 14:24:58 +0500

    change : /id page to ssr

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,16 +1,37 @@
-"use client";
+import { IMovieDetailsResponse } from "@/api/movies/movies.types";
 import { FC } from "react";
 import MovieDetails from "../_components/MovieDetails/MovieDetails";
-import { useMoviesGetByIdQuery } from "@/api/movies/movies.service";
 
 interface Params {
   id: string;
 }
 
-const SingleMoviePage: FC<{ params: Params }> = ({ params }) => {
-  const { data } = useMoviesGetByIdQuery(+params.id);
+const fetchMovieDetails = async (movieId: string) => {
+  const res = await fetch(
+    `https://api.cinerama.uz/test/movies/view?module_id=3&id=${movieId}`,
+    {
+      headers: {
+        Authorization: `Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM`,
+      },
+      cache: "force-cache",
+    }
+  );
 
-  return <MovieDetails details={data?.data} />;
+  if (!res.ok) {
+    throw new Error(`Server-side fetch failed with status ${res.status}`);
+  }
+
+  return res.json();
+};
+
+const SingleMoviePage: FC<{ params: Params }> = async ({ params }) => {
+  try {
+    const data: IMovieDetailsResponse = await fetchMovieDetails(params.id);
+
+    return <MovieDetails details={data.data} />;
+  } catch (error: any) {
+    return <div style={{ color: "white" }}>Error: {error.message}</div>;
+  }
 };
 
 export default SingleMoviePage;
