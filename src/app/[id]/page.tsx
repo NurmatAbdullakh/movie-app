c6d326ee1616e3bc966179a2b702cd756ed5328f
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-06-28 06:14:33 +0500

    refactoring

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -5,29 +5,32 @@ import MovieDetails from "../_components/MovieDetails/MovieDetails";
 interface Params {
   id: string;
 }
-export const dynamic = "force-dynamic";
+
+const fetchMovieDetails = async (movieId: string) => {
+  const res = await fetch(
+    `https://api.cinerama.uz/test/movies/view?module_id=3&id=${movieId}`,
+    {
+      headers: {
+        Authorization: `Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM`,
+      },
+      cache: "force-cache",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error(`Server-side fetch failed with status ${res.status}`);
+  }
+
+  return res.json();
+};
 
 const SingleMoviePage: FC<{ params: Params }> = async ({ params }) => {
   try {
-    const res = await fetch(
-      `https://api.cinerama.uz/test/movies/view?module_id=3&id=${params.id}`,
-      {
-        headers: {
-          Authorization: `Bearer DrTVm2Bi8pHE75xYsM94fjciuAhju2XM`,
-        },
-        cache: "force-cache",
-      }
-    );
-
-    if (!res.ok) {
-      throw new Error(`Server-side fetch failed with status ${res.status}`);
-    }
-
-    const data: IMovieDetailsResponse = await res.json();
+    const data: IMovieDetailsResponse = await fetchMovieDetails(params.id);
 
     return <MovieDetails details={data.data} />;
   } catch (error: any) {
-    return <div>Error: {error.message}</div>;
+    return <div style={{ color: "white" }}>Error: {error.message}</div>;
   }
 };
 
